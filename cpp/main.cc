9d9f6aa0766e9d6e0122d96c0f935fabe0230779
Jose Cardona <jose.cardona.externo@axa-assistance.es>
2025-08-26 14:18:51 +0200

    add csharp version w .net9

--- cpp/main.cc
+++ cpp/main.cc
@@ -8,6 +8,7 @@ int main() {
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
     for (int i = 0; i < image_width; i++) {
       double r = double(i) / (image_width - 1);
       double g = double(j) / (image_height - 1);
@@ -20,4 +21,6 @@ int main() {
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
+
+  std::clog << "\rDone.                                    \n";
 }
---
+++ dotnet/Program.cs
@@ -0,0 +1,30 @@
+﻿int imageWidth = 256;
+int imageHeight = 256;
+
+Console.WriteLine($"P3");
+Console.WriteLine($"{imageWidth} {imageHeight}");
+Console.WriteLine(255);
+
+var standardError = new StreamWriter(Console.OpenStandardError());
+standardError.AutoFlush = true;
+Console.SetError(standardError);
+
+for (int j = 0; j < imageHeight; j++)
+{
+    Console.Error.Write($"\rScanlines remaining: {imageHeight - j}");
+
+    for (int i = 0; i < imageWidth; i++)
+    {
+        double r = Convert.ToDouble(i) / (imageWidth - 1);
+        double g = Convert.ToDouble(j) / (imageHeight - 1);
+        double b = 0.0;
+
+        int ir = Convert.ToInt32(255.999 * r);
+        int ig = Convert.ToInt32(255.999 * g);
+        int ib = Convert.ToInt32(255.999 * b);
+
+        Console.WriteLine("{0} {1} {2}", ir, ig, ib);
+    }
+}
+
+Console.Error.Write("\rDone.                            ");
